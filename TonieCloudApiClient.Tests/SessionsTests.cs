ea388253d0b6fc2df8db49d0479193ebe11c36c8
Marcus Radlach <m.radlach@mailbox.org>
2020-01-14 00:04:15 +0100

    first steps, working login

---
+++ TonieCloudApiClient.Tests/SessionsTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+using System.Net;
+using Should;
+using RestSharp;
+
+namespace TonieCloudApiClient.Tests
+{
+    public class SessionsTests
+    {
+        private const string UserName = "batzi.01+tonies@mailbox.org";
+        private const string Password = "IBGpM4eN7F9uWAFFK6c2";
+
+        [Test]
+        public async Task CanPostSessions()
+        {
+            var result = await SessionsClient.PostAsync(UserName, Password);
+
+            result.Jwt.ShouldNotBeNull();
+
+            //var meClient = new RestClient($"{Urls.BaseUrl}/me");
+            //meClient.AddDefaultHeader("Authorization", $"Bearer {result.Jwt}");
+
+            //var getRequest = new RestRequest();
+
+            //var meResult = await meClient.GetAsync<object>(getRequest);
+        }
+    }
+}
---
+++ TonieCloudApiClient/BearerTokenModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TonieCloudApiClient
+{
+    public class BearerTokenModel
+    {
+        public string Jwt { get; set; }
+    }
+}
